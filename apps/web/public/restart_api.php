ea68aceadb76d031af813fdf7428f6009c27564b
Seruni Mumbul <serunimumbul2010@gmail.com>
2026-09-06 04:58:02 +0700

    fix: remove exec from restart_api.php to avoid 500 error

--- apps/web/public/restart_api.php
+++ apps/web/public/restart_api.php
@@ -1,8 +1,18 @@
 <?php
 $target1 = '/home/u800256894/domains/indigo-barracuda-105731.hostingersite.com/hbuilds/current/nodejs/tmp/restart.txt';
 $target2 = '/home/u800256894/domains/indigo-barracuda-105731.hostingersite.com/hbuilds/current/tmp/restart.txt';
-@touch($target1);
-@touch($target2);
-exec("pgrep -u u800256894 -f 'domains/indigo-barracuda' | xargs kill -9 2>/dev/null");
-echo "Restart signal sent!";
+
+if (touch($target1)) {
+    echo "Touched $target1<br>";
+} else {
+    echo "Failed to touch $target1<br>";
+}
+
+if (touch($target2)) {
+    echo "Touched $target2<br>";
+} else {
+    echo "Failed to touch $target2<br>";
+}
+
+echo "Restart signal sent via touch!";
 ?>
